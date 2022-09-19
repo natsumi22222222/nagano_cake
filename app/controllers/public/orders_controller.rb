2db110928c78e5157b82256ba65390024b1ba759
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-09-19 11:40:54 +0000

    [Update]

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,13 @@
+class Public::OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,48 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/confirm'
+    get 'orders/complete'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   root "public/homes#top"
 
   devise_for :customers,skip: [:passwords], controllers: {
