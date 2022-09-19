e5b9e08fd2cd0b0fd7c0b4bb4cea5d40fcbf1e9d
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-09-19 09:50:21 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'homes/top'
-  devise_for :admins
-  devise_for :customers
-  
-  root "homes#top"
-  
+  root "public/homes#top"
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: 'admin/sessions'
+  }
+
+
+
+
+
 end
