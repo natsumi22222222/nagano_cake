f7a5542ca08a4cf150216c972a92ff1ba4a30efe
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-09-20 13:00:43 +0000

    [Update]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,20 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @item= Item.new
   end
 
   def new
+  end
+
+  def create
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
   end
 end
