c5c3244578a95f7bebc8627f83bc3888700696ba
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-10-05 13:22:18 +0000

    [Update]item index show layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resouce)
     case resource
     when Admin
-       new_admin_session_path
+      admin_session_path
     when Customer
       root_path
     end
