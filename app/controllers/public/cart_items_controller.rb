c5f671cf84dd9668e878c66fc0436fbe62f36e2d
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-10-13 07:32:26 +0000

    [Update]cart_item order new

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,8 +7,19 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item= current_customer.cart_items.new(cart_item_params)
-    @cart_item.save
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+
+    cart_item=  current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    cart_item.amount += params[:cart_item][:amount].to_i
+    cart_item.save
     redirect_to cart_items_path
+
+    elsif  @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      redirect_to cart_items_path
+    else
+      render "index"
+    end
   end
 
   def update
@@ -26,7 +37,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
-    @cart_items= current_customer.cart_items
+    @cart_items= CartItem.all
     @cart_items.destroy_all
     redirect_to cart_items_path
     flash[:notice]= 'カートが空になりました。'
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,8 @@
 class Public::OrdersController < ApplicationController
 
   def new
-    @order= Order.new
     @customer= current_customer
+    @addresses = @customer.addresses
   end
 
   def index
@@ -10,12 +10,25 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-    
+
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
   end
 
   def complete
+
   end
+
+private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
