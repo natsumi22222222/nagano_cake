1b26c4dacde722e6672efe6eca1584dc8713c778
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-10-21 13:26:59 +0000

    [Update]making status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,3 +1,27 @@
 class Admin::OrderDetailsController < ApplicationController
-  
+
+
+  def update
+    @order_detail= OrderDetail.find(params[:id])
+    @order= @order_detail.order
+    @order_details = @order.order_details
+    @order_detail.update(order_detail_params)
+
+    if @order_details.where(making_status: "製作中").count >= 1
+      @order.status= "製作中"
+      @order.save
+    end
+
+    if @order_details.where(make_status: "製作完了").count
+      @order.status= "発送準備中"
+      @order.save
+    end
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,28 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @total= 0
     @order= Order.find(params[:id])
-    @order_details=OrderDetail.all
+    @order_details=@order.order_details
   end
+
+  def update
+    @order= Order.find(params[:id])
+    @order.update(order_params)
+    @order_details= @order.order_details
+
+    if @order.status == "入金確認"
+      @order_details.each do |order_detail|
+        @order_detail.making_status = "製作待ち"
+        @order_detail.save
+      end
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
